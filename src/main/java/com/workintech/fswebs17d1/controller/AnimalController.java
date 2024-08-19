ba5d1c93cb74a47bc2a77afcd654cd2ba48b98aa
Mahmut Aktaş <mahmutaktas.m@gmail.com>
2024-08-19 17:00:31 +0300

    edited

--- src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/workintech/animal")
 public class AnimalController {
 
     @Value("${course.name}")
@@ -20,28 +21,28 @@ public class AnimalController {
 
     private Map<Integer, Animal> animals = new HashMap<>();
 
-    @GetMapping("/workintech/animal")
+    @GetMapping
     public List<Animal> findAll(){
         return animals.values().stream().toList();
     }
 
-    @GetMapping("/workintech/animal/{id}")
+    @GetMapping("/{id}")
     public Animal findById(@PathVariable int id){
         return animals.get(id);
     }
 
-    @PostMapping("/workintech/animal")
+    @PostMapping
     public void save(@RequestBody Animal animal){
         animals.put(animal.getId(), animal);
     }
 
-    @PutMapping("/workintech/animal/{id}")
+    @PutMapping("/{id}")
     public Animal save(@PathVariable int id, @RequestBody Animal animal){
         animals.put(id, animal);
         return animals.get(id);
     }
 
-    @DeleteMapping("/workintech/animal/{id}")
+    @DeleteMapping("/{id}")
     public Animal save(@PathVariable int id){
         return animals.remove(id);
     }
